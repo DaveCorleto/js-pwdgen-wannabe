3635a95811496b251f48b91b65b2c17e7c30821b
DaveCorleto <davide.corleto@fastwebnet.it>
2023-11-16 15:33:50 +0100

    boilerplate

---
+++ js/javascript.js
@@ -0,0 +1,20 @@
+// Ciao ragazzi, esercizio di oggi:
+// (insicurissimo) password generator
+// nome repo: js-pwdgen-wannabe
+// Chiedi all’utente il suo nome, poi chiedi il suo cognome, poi chiedi il suo colore preferito
+// Infine scrivi sulla pagina nomecognomecolorepreferito23.
+// Buon coding … :baby-yoda:
+// io spero di riuscire a stare zitto per il resto della giornata
+
+
+// Primo tentativo :)
+// document.writeln('Helloworld!');
+
+document.getElementById('prova').innerHTML='Hello World!'
+
+const nome = 'Davide';
+const cognome = 'Corleto';
+const colorepreferito = 'Rosso';
+
+
+console.log("prova da console");
